e0776e2ed99f7fa4009e0bba5929f24652070328
Sam Adams <sam.adams7785@gmail.com>
2023-01-26 17:28:52 -0800

    styled the board

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Board.css';
+import Box from '../Box/Box.js';
+
+import { UserContext } from '../../context/GameContext.js';
 
 export default function Board() {
-  return <div>Board</div>;
+  const { boxes } = useContext(UserContext);
+  return (
+    <>
+      <h1 className="header">REACT TIC-TAC-TOE</h1>
+      <div className="outline">
+        <h1 className="turn">It&apos;s (Player) Turn</h1>
+        <h2 className="winner">(PLAYER) WINS</h2>
+        <div className="board-container">
+          {boxes.map((box) => (
+            <Box key={box.space} space={box.space} content={box.content} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Components/Box/Box.js
+++ src/Components/Box/Box.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './Box.css';
 
 export default function Box() {
-  return <div>Box</div>;
+  return <div className="space"></div>;
 }
